5f84d13e0af670dff182f9c27e0ad7f1aae7a0f7
Zalillo18 <gmcano85@gmail.com>
2020-09-19 11:54:04 +0200

    para borrorar

--- routes/user-logged.routes.js
+++ routes/user-logged.routes.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-const checkLoggedIn = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { message: 'Desautorizado, incia sesión para continuar' })
+const checkLoggedIn = (req, res, next) => {
+
+    const userRegistered = true
+    if (req.isAuthenticated()) {
+        //console.log("Entro---------------------------------", req.user.username)
+        // console.log("Entro---------------------------------", req.params.username)
+        if(req.user.username === req.params.username)
+            res.render('profile', { user: req.user, userRegistered })
+    }
+    else 
+       next()
+    
+       // res.render('auth/login', { message: 'Desautorizado, inicia sesión para continuar' })
+}
+
 // const checkRole = rolesToCheck => (req, res, next) => req.isAuthenticated() && rolesToCheck.includes(req.user.role) ? next() : res.render('auth/login', { message: 'Desautorizado, no tienes permisos para ver eso.' })
 
 const checkRole = rolesToCheck => {
@@ -16,15 +30,15 @@ const checkRole = rolesToCheck => {
 }
 
 
-router.get('/profile', checkLoggedIn, (req, res, next) => { 
+router.get('/:username', checkLoggedIn, (req, res, next) => { 
     // const user = req.user
-    const userRegistered = true
+    // const userRegistered = true
     
-
-    res.render('profile', { user: req.user, userRegistered })
+    res.render('profile', req.query)
+    // res.render('profile', { user: req.user })
 })
 
-router.get('/profile/?', (req, res) => res.render('profile', req.query))
+// router.get('/username', (req, res) => res.render('profile', req.query))
 
 
 module.exports = router
