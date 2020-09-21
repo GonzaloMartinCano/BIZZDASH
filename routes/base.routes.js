f06ec7817e3b21092082c4e11a6bcd05b060ccc0
Zalillo18 <gmcano85@gmail.com>
2020-09-21 13:04:54 +0200

    Cloudinary

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/user.model')
+
 router.get('/', (req, res) => res.render('index'))
+
+router.post('/', (req, res) => {
+
+    let username = {}
+    if (req.body.username)
+        username = { username: req.body.username }
+    
+    User.find(username)
+    //.then((users) => console.log(users))
+    .then((users) => res.render('index', {users}))
+    .catch((err) => console.log(err))
+
+    
+})
+
 
 
 
